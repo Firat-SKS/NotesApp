e37848b651479e8f61696d3fd8a224267e0f4692
adematici <adematicis41@gmail.com>
2025-01-27 04:32:34 +0300

    Add dimensions and colors

--- app/src/main/java/com/orkhontech/notesapp/ui/theme/Color.kt
+++ app/src/main/java/com/orkhontech/notesapp/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.orkhontech.notesapp.ui.theme
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,31 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+@Stable
+val Color.Companion.C_BLACK: Color
+    get() = Color(0xFF000000)
+
+@Stable
+val Color.Companion.C_WHITE: Color
+    get() = Color(0xFFFFFFFF)
+
+@Stable
+val Color.Companion.C_LIGHT_GRAY: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+val Color.Companion.C_BLUE: Color
+    get() = Color(0xFF007BFF)
+
+@Stable
+val Color.Companion.C_GREEN: Color
+    get() = Color(0xFF28A745)
+
+@Stable
+val Color.Companion.C_ORANGE: Color
+    get() = Color(0xFFFF5733)
+
+@Stable
+val Color.Companion.C_YELLOW: Color
+    get() = Color(0xFFFFC107)
